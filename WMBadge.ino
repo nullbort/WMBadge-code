4c0ba2eaeca573bdcfe10dae52831860d06aba74
Chris Ritchie <nullbort@gmail.com>
2019-04-13 19:23:54 -0400

    Assign led variables to match pcb

--- WMBadge.ino
+++ WMBadge.ino
@@ -32,6 +32,7 @@ byte pins[] = {ledpin1, ledpin2, ledpin3};
 //initialize object
 Charlieplex charlieplex = Charlieplex(pins , NUMBER_OF_PINS);
 
+/*
 //individual 'pins' , adress charlieplex pins as you would adress an array
 charliePin led1 = { 0 , 1 }; // Set the pins the LEDs are connected to.
 charliePin led2 = { 1 , 2 }; // If the wrong LED lights, change it here.
@@ -39,7 +40,13 @@ charliePin led3 = { 2 , 0 };
 charliePin led4 = { 0 , 2 };
 charliePin led5 = { 2 , 1 }; 
 charliePin led6 = { 1 , 0 };
-
+*/
+charliePin led1 = { 0 , 1 };
+charliePin led2 = { 1 , 2 };
+charliePin led3 = { 0 , 2 };
+charliePin led4 = { 2 , 0 };
+charliePin led5 = { 2 , 1 }; 
+charliePin led6 = { 1 , 0 };
 
 void setup(){ 
   /*nothing*/
@@ -70,27 +77,7 @@ void loop(){
   charlieplex.charlieWrite(led6,HIGH);
   delay(1000);
   charlieplex.clear();
- 
-  long startTime = millis();
-  while ( millis() - startTime < 5000)
-    { 
-      
-      // This lights each LED (or doesn't light it) for a split second. 
-      // No two LEDs are ever on at the same time. 
-        charlieplex.charlieWrite(led1, HIGH);
-        charlieplex.clear();
-        charlieplex.charlieWrite(led2, HIGH);
-        charlieplex.clear();
-        charlieplex.charlieWrite(led3, HIGH);
-        charlieplex.clear();
-        charlieplex.charlieWrite(led4, HIGH);
-        charlieplex.clear();
-        charlieplex.charlieWrite(led5, HIGH);
-        charlieplex.clear();
-        charlieplex.charlieWrite(led6, HIGH);
-        charlieplex.clear();
-    }    
-
+  
 }
 
 
